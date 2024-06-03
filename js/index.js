5da94bcdc391b1f44ff6960a35061633eff456a0
Phil <philgdaum@gmail.com>
2024-06-02 18:54:20 -0600

    initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// This function is connected to the hero button and 'design' button
+// those are the spots where the function is nested in the html
+// can change later to launch app, RN it just does the rickroll
+
+function launchApp() {
+    window.location.href = "rickroll.html";
+} 
+
